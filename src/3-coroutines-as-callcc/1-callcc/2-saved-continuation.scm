9ab5701d34c9b8b0bab949c0ec1232aecf756e31
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-04-10 19:38:20 +0100

    simplified saved-continuation example

--- src/3-coroutines-as-callcc/1-callcc/2-saved-continuation.scm
+++ src/3-coroutines-as-callcc/1-callcc/2-saved-continuation.scm
@@ -8,17 +8,14 @@
 
             (define (main args)
               (define count 0)
-              (log 1)
 
               (log (+ 100 (call/cc (lambda (continuation)
                  (set! saved-continuation continuation)
-                 (log 2)
-                 (continuation 3)
+                 (continuation 100)
                  (log "🙈")
               ))))
-              (log 4)
 
-              (if (< count 13) (begin
+              (if (< count 12) (begin
                  (set! count (+ 1 count))
                  (log "🚀")
                  (saved-continuation count)
